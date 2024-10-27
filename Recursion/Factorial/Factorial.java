ce89ed23b02a2bbe97b4bee07536601486a4b02f
Shrawan2002 <shrawankumar1754@gmail.com>
2024-10-27 14:10:39 +0530

    initial files

--- Recursion/Factorial/Factorial.java
+++ Recursion/Factorial/Factorial.java
@@ -1,15 +1,17 @@
 
-public class Factorial {
-    public static int fact(int n){
-        if (n==0) {
+
+ public class factorial {
+
+    public static int getFact(int n){
+        if (n==1) {
             return 1;
         }
-        int fnm1 = fact(n-1);
+        int fnm1 = getFact(n-1);
         int fn = n*fnm1;
         return fn;
     }
     public static void main(String[] args) {
-        int n= 5;
-        System.out.println(fact(n));
+        int n =5;
+        System.out.println(getFact(n));
     }
-}
+ }
