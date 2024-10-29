03238908194ac49150eb4e563ad97c80e95ff265
Shrawan2002 <shrawankumar1754@gmail.com>
2024-10-29 14:17:34 +0530

    initial files

---
+++ Recursion/PrintExponential/GetExponential.java
@@ -0,0 +1,18 @@
+
+
+public class GetExponential {
+
+    public static int getPow(int x , int n){
+        if (n == 0) {
+            return 1;
+        }
+        // int xnm1 = getPow(x, n-1);
+        // int xn = x*xnm1;
+        // return xn;
+
+        return x*
+    }
+    public static void main(String[] args) {
+
+    }
+}
