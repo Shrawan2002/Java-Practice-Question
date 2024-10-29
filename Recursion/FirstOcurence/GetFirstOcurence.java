26d10b2aa1aed359c53fb0eda555c1445d6acb32
Shrawan2002 <shrawankumar1754@gmail.com>
2024-10-29 12:52:53 +0530

    initial files

--- Recursion/FirstOcurence/GetFirstOcurence.java
+++ Recursion/FirstOcurence/GetFirstOcurence.java
@@ -1,14 +1,18 @@
-package Recursion.FirstOcurence;
+
 
 public class GetFirstOcurence {
     public static int firstOcurence(int arr[], int key, int i){
+        if (i == arr.length-1) {
+            return -1;
+        }
         if (arr[i] ==key) {
             return i;
         }
 
-        return 
+        return firstOcurence(arr, key, i+1);
     }
     public static void main(String[] args) {
-        int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+        int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 11};
+        System.out.println(firstOcurence(arr, 5, 0));
     }
 }
