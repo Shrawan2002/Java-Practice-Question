5ed1489ae1053a1e8f481795af77f50d8a0088c7
Shrawan2002 <shrawankumar1754@gmail.com>
2024-11-14 10:00:46 +0530

    initial files

--- Array_Question/Sub_Arr/PrintSubArr.java
+++ Array_Question/Sub_Arr/PrintSubArr.java
@@ -1,6 +1,8 @@
 
 public class PrintSubArr {
     public static void printSubarray(int arr[]){
+        int ts = 0;
+
         for(int i = 0; i<arr.length; i++){
             int start = i;
             for(int j = i ; j<arr.length; j++){
@@ -8,10 +10,12 @@ public class PrintSubArr {
                 for(int k = start; k<=end; k++){
                     System.out.print(arr[k] + " ");
                 }
+                ts++;
                 System.out.println(" ");
             }
             System.out.println(" ");
         }
+        System.out.println("total subArr =" + ts);
     }
     public static void main(String[] args) {
         int arr [] = {2, 4, 6, 8, 10, 12};
