31dfdd14e8f3a8efa113db84b4bfa2a50d27e1be
Shrawan2002 <shrawankumar1754@gmail.com>
2024-11-16 09:59:20 +0530

    initial files

--- Array_Question/PrifixSum/Prefix_Sum.java
+++ Array_Question/PrifixSum/Prefix_Sum.java
@@ -10,10 +10,15 @@ public class Prefix_Sum {
 
         int prefixSumArr [] = new int[n];
         prefixSumArr[0] = arr[0];
+
+        for(int i =1; i<prefixSumArr.length; i++){
+            prefixSumArr[i] = prefixSumArr[i-1] + arr[i];
+        }
         for(int i = 0; i<arr.length; i++){
             int start = i;
             for(int j = i; j<arr.length; j++){
-
+                int end = j;
+                curSum = start == 0? prefixSumArr[end] : prefixSumArr[end] - prefixSumArr[start - 1];
             }
         }
     }
