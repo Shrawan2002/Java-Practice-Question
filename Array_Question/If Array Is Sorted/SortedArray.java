714df3816a32c75ad07c892867a9440d9974f936
Shrawan2002 <shrawankumar1754@gmail.com>
2024-11-19 16:28:44 +0530

    initial files

---
+++ Array_Question/If Array Is Sorted/SortedArray.java
@@ -0,0 +1,18 @@
+
+public class SortedArray {
+
+    public static boolean checkSortedArray(int arr[]){
+        for(int i = 1; i<arr.length; i++){
+            if (arr[i] >= arr[i-1]) {
+                
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 3, 4, 4};
+        System.out.println(checkSortedArray(arr));
+    }
+}
