be44172c72b01ae96eb94843340b2dd019b16dec
Shrawan2002 <shrawankumar1754@gmail.com>
2024-11-20 23:22:55 +0530

    initial files

--- Array_Question/Move Zero/Batter Solution/MoveZero.java
+++ Array_Question/Move Zero/Batter Solution/MoveZero.java
@@ -1,34 +1,26 @@
 
 public class MoveZero {
     public static void moveZero(int arr[]){
-        int n = arr.length;
-        int temp[] = new int[n];
         int j = 0;
         for(int i = 0; i<arr.length; i++){
             if (arr[i] != 0) {
-                temp[j] = arr[i];
+                arr[j] = arr[i];
                 j++;
             }
         }
-        for(int i = 0; i<temp.length; i++){
-            arr[i] = temp[i];
-        }
-        for(int i = temp.length; i<n; i++ ){
+
+        for(int i = j ; i<arr.length; i++){
             arr[i] = 0;
         }
     }
-
     public static void printArray(int arr[]){
         for(int i = 0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
         }
     }
     public static void main(String[] args) {
-        int arr[] = {0, 1, 0, 3, 4, 0, 7};
+        int arr[] = {1, 0, 1, 0, 2, 3, 0};
         moveZero(arr);
         printArray(arr);
     }
 }
-
-// time complexity = O(n) + O(x) + O(n-x) = O(2n)
-// Space complexity = O(N)
