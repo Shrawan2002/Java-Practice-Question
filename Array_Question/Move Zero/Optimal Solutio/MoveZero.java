d4e9bfc2ca9fb1c2b528fafa586d2611b5a1da5b
Shrawan2002 <shrawankumar1754@gmail.com>
2024-11-23 21:00:50 +0530

    initial files

--- Array_Question/Move Zero/Optimal Solutio/MoveZero.java
+++ Array_Question/Move Zero/Optimal Solutio/MoveZero.java
@@ -12,7 +12,10 @@ public class MoveZero {
             }
         }
     }
-   
+    public static void printArray(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
     public static void main(String[] args) {
         int arr[] = {1, 0, 3, 0, 4, 5, 6, 0};
