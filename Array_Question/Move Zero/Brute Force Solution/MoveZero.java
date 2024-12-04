5f9d16370bb807471600158ce3da8284779c347e
Shrawan2002 <shrawankumar1754@gmail.com>
2024-12-04 20:46:18 +0530

    initial files

--- Array_Question/Move Zero/Brute Force Solution/MoveZero.java
+++ Array_Question/Move Zero/Brute Force Solution/MoveZero.java
@@ -18,10 +18,10 @@ public class MoveZero {
         }
     }
 
-    public static void printArray(int arr[]){
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+    // public static void printArray(int arr[]){
+    //     for(int i = 0; i<arr.length; i++){
+    //         System.out.print(arr[i] + " ");
+    //     }
     }
     public static void main(String[] args) {
         int arr[] = {0, 1, 0, 3, 4, 0, 7};
