4bbe76648115ee87ef7bc9aeb5e4c85089295021
Shrawan2002 <shrawankumar1754@gmail.com>
2024-12-10 21:11:58 +0530

    initial files

--- String Question/UpperCase/ToUpperCase.java
+++ String Question/UpperCase/ToUpperCase.java
@@ -1,16 +1,21 @@
-package String Question.UpperCase;
 
 public class ToUpperCase {
     public static String toUpper(String str){
         StringBuilder sb = new StringBuilder(" ");
         sb.append(Character.toUpperCase(str.charAt(0)));
         for(int i = 1; i<str.length(); i++){
-            if (condition) {
-                
+            if (str.charAt(i) ==' ' && i<str.length()-1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else{
+                sb.append(str.charAt(i));
             }
         }
+        return sb.toString();
     }
     public static void main(String[] args) {
         String str = "Hi i am shrawan";
+        System.out.println(toUpper(str));
     }
 }
