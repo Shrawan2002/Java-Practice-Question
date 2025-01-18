ee9baae2d404792c705ef9dcba6e964615f0a3db
Shrawan2002 <shrawankumar1754@gmail.com>
2025-01-18 13:22:57 +0530

    initial files

---
+++ Collection FrameWork/MAP/ImplementationOfHashMap.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.HashMap;
+public class ImplementationOfHashMap {
+    static class HashMap<K, V>{
+        private class Node {
+            K key;
+            V value;
+            public node
+        }
+    }
+}
