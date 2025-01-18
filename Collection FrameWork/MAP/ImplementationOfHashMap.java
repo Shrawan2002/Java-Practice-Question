ec5603b156be62e592b8d93ea012d660e009aa0e
Shrawan2002 <shrawankumar1754@gmail.com>
2025-01-18 13:42:47 +0530

    initial files

--- Collection FrameWork/MAP/ImplementationOfHashMap.java
+++ Collection FrameWork/MAP/ImplementationOfHashMap.java
@@ -1,11 +1,17 @@
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 public class ImplementationOfHashMap {
     static class HashMap<K, V>{
         private class Node {
             K key;
             V value;
-            public node
+            public Node(K key, V value){
+                this.key = key;
+                this.value = value;
+            }
         }
+        private int n; // number of nodes
+        private int N; // number of buckets
+        private LinkedList<Node> buckets[];
+
     }
 }
